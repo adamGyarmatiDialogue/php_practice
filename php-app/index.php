65a9647d25d247a2295f41041d6c5a70f0335ad4
Adam Gyarmati <adam.gyarmati@dialoguecreatives.com>
2024-06-06 16:00:58 +0200

    first commit

---
+++ php-app/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require 'functions.php';
+
+$uri = $_SERVER['REQUEST_URI'];
+
+echo $uri;
+
+if ($uri === "/") {
+	require "./controllers/index.php";
+} else if ($uri === "/about") {
+	require "./controllers/about.php";
+} else if ($uri === "/contact") {
+	require "./controllers/contact.php";
+}
